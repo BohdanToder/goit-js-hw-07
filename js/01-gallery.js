9d9294e676ac7d358c064bda1209e26e4b1b1a79
BohdanToder <bohdantoder7454@gmail.com>
2022-01-25 16:54:16 +0200

    Adds first code

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryItems.forEach(image => {
+    const imageWrapper = document.createElement('div');
+    imageWrapper.classList.add('gallery__item');
+
+    const imageLinkEL = document.createElement('a');
+    imageLinkEL.classList.add('gallery__link');
+    imageLinkEL.href = image.original;
+
+    const imageEL = document.createElement('img');
+    imageEL.classList.add('gallery__image');
+    imageEL.src = image.preview;
+    imageEL.dataset.source = image.original;
+    imageEL.alt = image.description;
+
+    imageLinkEL.append(imageEL);
+    imageWrapper.append(imageLinkEL);
+    galleryEl.append(imageWrapper);
+});
+
+const test = (event) => {
+    event.preventDefault();
+
+    let test1 = event.target.dataset.source;
+
+    // console.log(test1);
+    // console.log(event.target);
+};
+
+galleryEl.addEventListener('click', test);
